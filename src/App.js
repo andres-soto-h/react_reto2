3c019c5f34c189f3356e92f422c6350618540288
Andrés Soto <andres.soto.h@outlook.com>
2019-01-19 17:40:51 -0500

    Add states and functionallity

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 0,
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({clicks: this.state.clicks + 1});
+  }
+
+  DecreaseItem = () => {
+    this.setState({clicks: this.state.clicks - 1});
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{ this.state.clicks }</span>
+        <button id="inc" onClick={this.IncrementItem}>Incrementa</button>
       </div>
     );
   }
+
+
 }
 
 export default App;
